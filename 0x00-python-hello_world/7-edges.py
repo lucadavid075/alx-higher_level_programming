e70fd0f1ea336167bfdca13ce3cb40a9f85dc128
Luke David <daiveedlucas049@gmail.com>
2022-05-31 02:16:42 +0000

    Added files for pyhton hello world submission

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+first_3_letters = word[:3]
+last_2_letters = word[-2:]
+middle_word = word[1:-1]
+print("First 3 letters: {}".format(first_3_letters))
+print("Last 2 letters: {}".format(last_2_letters))
+print("Middle word: {}".format(middle_word))
