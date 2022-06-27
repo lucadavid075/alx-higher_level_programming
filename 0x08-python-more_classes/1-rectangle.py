d992d97147732a16e5897093fbb0b50a75acd871
Luke David <daiveedlucas049@gmail.com>
2022-06-27 23:17:37 +0000

    added files for python more classes

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+class Rectangle:
+    """ Class that creates a Rectangle Object """
+    def __init__(self, width=0, height=0):
+        """ Constructor """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """ Width Getter """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ Width Setter """
+        if not isinstance(value, int):
+            msg = "width must be an integer"
+            raise TypeError(msg)
+        if (value < 0):
+            msg = "width must be >= 0"
+            raise ValueError(msg)
+        self.__width = value
+
+    @property
+    def height(self):
+        """ Height Getter """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ Height Setter """
+        if not isinstance(value, int):
+            msg = "height must be an integer"
+            raise TypeError(msg)
+        if (value < 0):
+            msg = "height must be >= 0"
+            raise ValueError(msg)
+        self.__height = value
