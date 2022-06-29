bb81a124a5ca651b33a890e868f6e45788f2595d
Luke David <daiveedlucas049@gmail.com>
2022-06-29 04:30:26 +0000

    added files for test driven dev tasks

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,20 +1,14 @@
 #!/usr/bin/python3
-"""Module for multiplying two matrices with numpy
-"""
-
+"""Defines a matrix multiplication function using NumPy."""
 import numpy as np
 
 
 def lazy_matrix_mul(m_a, m_b):
-    """Function that multiplies 2 matrices
+    """Return the multiplication of two matrices.
 
     Args:
-
-    m_a: matrix a
-    m_b: matrix b
-
-    -m_a and m_b must be a list of lists of ints or floats
-    Else an error is raised
+        m_a (list of lists of ints/floats): The first matrix.
+        m_b (list of lists of ints/floats): The second matrix.
     """
 
     return (np.matmul(m_a, m_b))
--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -1,37 +1,28 @@
-#include <stdio.h>
-#include <Python.h>
-#include <string.h>
+#include "Python.h"
 
 /**
- * print_python_string - prints some basic info about Python string
- *
- * @p: Object of python
- *
- * Return: No Return
+ * print_python_string - Prints information about Python strings.
+ * @p: A PyObject string object.
  */
 void print_python_string(PyObject *p)
 {
-	ssize_t len;
-	wchar_t *str;
+	long int length;
+
+	fflush(stdout);
 
 	printf("[.] string object info\n");
-
-	if (strcmp(p->ob_type->tp_name, "str"))
+	if (strcmp(p->ob_type->tp_name, "str") != 0)
 	{
 		printf("  [ERROR] Invalid String Object\n");
 		return;
 	}
 
+	length = ((PyASCIIObject *)(p))->length;
+
 	if (PyUnicode_IS_COMPACT_ASCII(p))
 		printf("  type: compact ascii\n");
 	else
 		printf("  type: compact unicode object\n");
-
-	len = PyUnicode_GET_LENGTH(p);
-
-	printf("  length: %ld\n", len);
-
-	str = PyUnicode_AsWideCharString(p, &len);
-
-	printf("  value: %ls\n", str);
+	printf("  length: %ld\n", length);
+	printf("  value: %ls\n", PyUnicode_AsWideCharString(p, &length));
 }
