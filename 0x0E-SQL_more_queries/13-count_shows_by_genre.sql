ede35c8bdb0a4997a8a65558bbdb3dfb53292caa
Luke David <daiveedlucas049@gmail.com>
2022-08-11 04:13:35 +0000

    [feat] cript that lists all genres and displays the number of shows linked to each

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,8 @@
+-- Lists all shows contained in hbtn_0d_tvshows without a genre linked.
+-- Query to perform operation
+SELECT gr.name AS genre,
+       COUNT(shgr.genre_id) AS number_of_shows FROM tv_genres AS gr
+       JOIN tv_show_genres AS shgr
+       ON gr.id=shgr.genre_id
+       GROUP BY shgr.genre_id
+       ORDER BY number_of_shows DESC;
