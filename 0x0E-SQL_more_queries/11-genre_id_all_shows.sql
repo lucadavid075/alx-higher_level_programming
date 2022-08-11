567aa77cd2906a151b02e7eafd612482f6182938
Luke David <daiveedlucas049@gmail.com>
2022-08-11 04:10:53 +0000

    [feat] script that lists all shows contained in the database hbtn_0d_tvshows on server

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,5 @@
+-- Lists all shows contained in the database hbtn_0d_tvshows.
+-- Query to perform operation
+SELECT sh.title, shgr.genre_id FROM tv_shows AS sh
+       LEFT JOIN tv_show_genres AS shgr ON sh.id=shgr.show_id
+       ORDER BY sh.title, shgr.genre_id ASC;
