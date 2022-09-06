92118b9ca1517775530075ef0087c0b705aa2f7c
Luke David <daiveedlucas049@gmail.com>
2022-09-06 03:26:07 +0000

    [fix] requirements for task 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const lang = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const lang = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (const i of lang) {
   console.log(i);
 }
