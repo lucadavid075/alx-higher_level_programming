ba6f5d62248d2b83b43ba1a7f7ca28b629aa0f41
Luke David <daiveedlucas049@gmail.com>
2022-09-09 04:27:13 +0000

    [feat] lists all states from the database

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+import sys
+import MySQLdb
+
+def get_states(username, password, db_name):
+    """List all states from the database hbtn_0e_0_usa"""
+    
+    conn = MySQLdb.connect(host="localhost", user=username, passwd=password, db=db_name, charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM `states` ORDER BY id ASC")
+    query_rows = cursor.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
+    
+    if __name__ == "__main__":
+        credentials = sys.argv
+        username = sys.argv[1]
+        passwd = sys.argv[2]
+        db_name = sys.argv[3]
+        get_states(iusername, password, db_name)
