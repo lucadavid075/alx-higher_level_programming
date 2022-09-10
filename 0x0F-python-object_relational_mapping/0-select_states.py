63ceedeb0c508bfed22e7d1430f438ff251c1e5d
Luke David <daiveedlucas049@gmail.com>
2022-09-10 20:09:44 +0100

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,26 +2,28 @@
 import sys
 import MySQLdb
 
+
 def get_states(username, password, db_name):
-    """List all states from the database hbtn_0e_0_usa"""
-    
-    conn = MySQLdb.connect(host="localhost",
-                           port=3306,
-                           user=username,
-                           passwd=password,
-                           db=db_name,
-                           charset="utf8")
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM `states` ORDER BY id ASC")
-    query_rows = cursor.fetchall()
-    for row in query_rows:
+    """
+        List all the states in the given database
+    """
+    db = MySQLdb.connect(host="localhost",
+                         user=username,
+                         passwd=password,
+                         db=db_name,
+                         port=3306)
+
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM `states` ORDER BY id ASC")
+    rows = cursor.fetchall()
+    for row in rows:
         print(row)
-    cur.close()
-    conn.close()
-    
-    if __name__ == "__main__":
-        credentials = sys.argv
-        username = sys.argv[1]
-        passwd = sys.argv[2]
-        db_name = sys.argv[3]
-        get_states(username, passwd, db_name)
+    cursor.close()
+    db.close()
+
+if __name__ == "__main__":
+    credentials = sys.argv
+    username = sys.argv[1]
+    passwd = sys.argv[2]
+    db_name = sys.argv[3]
+    get_states(username, passwd, db_name)
